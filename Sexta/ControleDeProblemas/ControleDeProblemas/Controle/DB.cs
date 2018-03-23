2e3d290f88c7989eea33543958336d06182c4833
Aluno Fiar Logatti <aluno@fiar.local>
2018-03-23 20:05:30 -0300

    Novo

---
+++ Sexta/ControleDeProblemas/ControleDeProblemas/Controle/DB.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controle
+{
+    public class DB
+    {
+        private SqlConnection conexao;
+
+        public DB()
+        { 
+            conexao = new SqlConnection
+            (
+                ConfigurationManager.ConnectionStrings["ConexaoSQLServer"].ConnectionString);
+            conexao.Open();
+        }
+    }
+}
